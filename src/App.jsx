b5d85a5570de54acfc8fac16bcbc7690e09aba64
Bradley Chery <Bradley434@hotmail.com>
2024-05-27 18:25:14 -0400

    updated delete route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import SearchAppBar from './components/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import GetAll from './components/GetAll.jsx'
 import AddPlayers from './components/AddPlayers'
-import DeletePlayers from './components/DeletePlayers'
 import PlayerDetail from './components/PlayerDetail'
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
           <Route path={'/'} element={<GetAll />} />
           <Route path={'/addPlayer'} element={<AddPlayers />} />
           <Route path={'/player/:id'} element={<PlayerDetail />} />
-          <Route path={'/deletePlayer'} element={<DeletePlayers />} />
         </Routes>
       </Router>
     </>
